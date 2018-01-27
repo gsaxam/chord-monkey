e143c8d553acd4fcd58a9aa44c1287b7d2ef7426
Saksham Ghimire <saksham.ghimire@charter.com>
2018-01-27 15:45:31 -0700

    add strum patterns display using FontAwesome and color changes on the UI theme

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
-import Home from './home';
-import { Font } from 'expo';
+import React from "react";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import Home from "./home";
+import { Font } from "expo";
+import FontAwesome, { Icons } from "react-native-fontawesome";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,9 @@ export default class App extends React.Component {
     await Expo.Font.loadAsync({
       lobster: require("./assets/fonts/Lobster/Lobster-Regular.ttf"),
       aclonica: require("./assets/fonts/Aclonica/Aclonica-Regular.ttf"),
-      tangerine: require("./assets/fonts/Tangerine/Tangerine-Regular.ttf")
+      tangerine: require("./assets/fonts/Tangerine/Tangerine-Regular.ttf"),
+      sofia: require("./assets/fonts/Sofia/Sofia-Regular.ttf"),
+      FontAwesome: require("./assets/fonts/fontawesome-webfont.ttf")
     });
     this.setState({ loading: false });
   }
@@ -22,33 +25,53 @@ export default class App extends React.Component {
     }
     return (
       <View style={styles.container}>
-      {/** <SvgUri width="200" height="200" source={require('./homer-simpson.svg')} /> */}
+        {/** <SvgUri width="200" height="200" source={require('./homer-simpson.svg')} /> */}
         <Home />
         <View style={styles.lineBreak} />
-          <FlatList
+        <FlatList
           data={[
-              {key: 'Papa Kehtey Hain', value: 'Am D F G'},
-              {key: 'Neele Neele Ambar', value: 'Bb Em F#'},
-              {key: 'Paani Da Rang', value: 'E D A'},
-              {key: 'Jo Bheji Thie Dua', value: 'Bsus7 D7 F# Cm'},
-              {key: 'Pehli Najar ', value: 'Am D F G'},
-              {key: 'Jadu Hai Nasha', value: 'Am D F G'},
-              {key: 'Main Agar Kahoon', value: 'Am D F G'},
-              {key: 'Dil Deewana', value: 'Am D F G'},
-              {key: 'O Rey Piya', value: 'Am D F G'},
-            ]}
-            renderItem={({item}) =>
+            { key: "Papa Kehtey Hain", value: "Am D F G", strum: "DDU UDU" },
+            { key: "Neele Neele Ambar", value: "Bb Em F#", strum: "DDU UDU" },
+            { key: "Paani Da Rang Dekhe Ke", value: "E D A", strum: "DDU UDU" },
+            {
+              key: "Jo Bheji Thie Dua",
+              value: "Bsus7 D7 F# Cm",
+              strum: "DDU UDU"
+            },
+            { key: "Pehli Najar ", value: "Am D F G", strum: "DDU UDU" },
+            { key: "Jadu Hai Nasha", value: "Am D F G", strum: "DDU UDU" },
+            { key: "Main Agar Kahoon", value: "Am D F G", strum: "DDU UDU" },
+            { key: "Dil Deewana", value: "Am D F G", strum: "DDU UDU" },
+            { key: "O Rey Piya", value: "Am D F G", strum: "DDU UDU" }
+          ]}
+          renderItem={({ item }) => (
             <View style={styles.listItem}>
               <View style={styles.row_cell_chord_songTitle}>
                 <View style={styles.row_only}>
-                {item.value.split(" ").map(chord => <Text style={styles.row_chord}>{chord}</Text>)}
+                  {item.value.split(" ").map(chord => (
+                    <Text style={styles.row_chord}>
+                      {chord}
+                    </Text>
+                  ))}
                 </View>
                 <View Style={styles.lineBreak} />
                 <Text style={styles.row_song_title}>{item.key}</Text>
               </View>
-            </View>}
-            />
-          </View>
+              <View style={styles.row_only_bottom}>
+                {item.strum.split('').map(strum => (
+                  <Text style={styles.row_strum}>
+
+                  {strum == 'U' ? <FontAwesome>{Icons.arrowUp}</FontAwesome> : null }
+                  {strum == 'D' ? <FontAwesome>{Icons.arrowDown}</FontAwesome> : null }
+                  {strum == ' ' ? " " : null }
+
+                  </Text>
+                ))}
+              </View>
+            </View>
+          )}
+        />
+      </View>
     );
   }
 }
@@ -56,21 +79,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dedddd',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    flexDirection: 'column'
+    marginTop: 20,
+    backgroundColor: "#001a2d",
+    alignItems: "stretch",
+    justifyContent: "center",
+    flexDirection: "column"
   },
   listItem: {
     flex: 1,
     elevation: 1,
     borderRadius: 2,
-    backgroundColor: '#f7f7f7',
-    flexDirection: 'row',  // main axis
-    justifyContent: 'flex-start', // main axis
-    alignItems: 'center', // cross axis
-    paddingTop: 20,
-    paddingBottom: 20,
+    backgroundColor: "#0e2739",
+    flexDirection: "row", // main axis
+    justifyContent: "flex-start", // main axis
+    alignItems: "center", // cross axis
+    paddingTop: 10,
+    // paddingBottom: 20,
     paddingLeft: 18,
     paddingRight: 16,
     marginLeft: 14,
@@ -79,37 +103,48 @@ const styles = StyleSheet.create({
     marginBottom: 6
   },
   row_cell_chord_songTitle: {
-      flex: 1,
-      flexDirection: 'column'
-        },
+    flex: 1,
+    flexDirection: "column"
+  },
   row_chord: {
-    color: 'black',
     includeFontPadding: false,
     flex: 0,
-    fontSize: 16,
-    color: '#fff',
+    fontSize: 14,
+    color: "#fff",
+    paddingTop: 2,
     paddingRight: 5,
     paddingLeft: 5,
-
-    borderRadius: 7,
-    borderWidth: 2,
+    borderRadius: 3,
     marginRight: 5,
-    borderColor: '#226898',
-    backgroundColor: '#3598dc',
-    overflow: 'hidden'
+    borderColor: "#226898",
+    backgroundColor: "#ee3450",
+    overflow: "hidden",
+    fontFamily: "aclonica"
   },
+  row_strum: {
+    color: '#22982e',
+    padding: 1,
+    fontSize: 12,
+    },
   row_song_title: {
-    color: '#353535c7',
-    textAlignVertical: 'top',
+    color: "#029ecf",
+    textAlignVertical: "top",
     includeFontPadding: false,
     flex: 0,
     fontSize: 30,
-    fontFamily:'lobster'
+    fontFamily: "lobster",
+    marginTop: 5
   },
-  row_only:{
-    flexDirection: 'row'
+  row_only: {
+    flexDirection: "row"
   },
-  lineBreak:{
+  row_only_bottom: {
+    marginTop: 70,
+    paddingBottom: 10,
+    flexDirection: "row",
+    textAlign: "right"
+  },
+  lineBreak: {
     marginTop: 10
   }
 });
--- home.js
+++ home.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import {StyleSheet, View, Text, Image } from 'react-native';
-
+import React from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default class Home extends React.Component {
-  render(){
-    return(
-        <View style={styles.topBar}>
-          <Image style={{width: 60, height: 60}} source={require('./images/monkey.png')} />
-          <View style={styles.spacer} />
-          <Text style={styles.titleText}>Chord Monkey</Text>
-        </View>
-    )
+  render() {
+    return (
+      <View style={styles.topBar}>
+        <Image
+          style={{ width: 60, height: 60 }}
+          source={require("./images/monkey.png")}
+        />
+        <View style={styles.spacer} />
+        <Text style={styles.titleText}>Chord Monkey</Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  topBar:{
-    marginTop: 20,
-    backgroundColor: '#424242',
-    alignItems: 'center',
+  topBar: {
+    backgroundColor: "#063955",
+    alignItems: "center",
     padding: 10,
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 75
   },
   spacer: {
     marginRight: 10
   },
-  titleText:{
+  titleText: {
     marginTop: 5,
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#35d381',
-    fontFamily: 'aclonica'
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#fff",
+    fontFamily: "sofia"
   }
 });
